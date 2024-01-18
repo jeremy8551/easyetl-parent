51a021eb4eb5f3930fb8042068c11c7c5c6de616
jeremy8551 <jeremy8551@gmail.com>
2024-01-18 16:51:09 +0800

    优化代码

--- easyetl-database/src/main/java/icu/etl/database/JdbcConverterMapper.java
+++ easyetl-database/src/main/java/icu/etl/database/JdbcConverterMapper.java
@@ -14,7 +14,7 @@ public interface JdbcConverterMapper {
      * @param key 字段名，字段类型或字段位置信息
      * @return
      */
-    public boolean contains(String key);
+    boolean contains(String key);
 
     /**
      * 返回字段类型对应的数据类型转换器实例
@@ -23,6 +23,6 @@ public interface JdbcConverterMapper {
      * @param key 字段名，字段类型或字段位置信息
      * @return 返回 {@linkplain JdbcObjectConverter} 类 或 {@linkplain JdbcStringConverter} 类的实例
      */
-    public <E> E get(String key);
+    <E> E get(String key);
 
 }
